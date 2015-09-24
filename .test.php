d5fa4691f4099a6386918eacdc2db77c7811a364
John Drinkwater <john@nextraweb.com>
2015-09-24 16:47:02 +0100

    Include phpunit test for a pre-commit hook
    Another file pinched from SteamLinux, .test.php is authored by @xPaw,
    @johndrinkwater & @soeb

---
+++ .test.php
@@ -0,0 +1,148 @@
+<?php
+	require 'vendor/autoload.php';
+
+	use Seld\JsonLint\JsonParser;
+
+	function is_set( $variable ) {
+		return isset( $variable );
+	}
+
+	function is_npcommid( $variable ) {
+		return is_string( $variable );
+	}
+
+	class PSNSteamTest extends PHPUnit_Framework_TestCase {
+
+		public function testFileExists( ) {
+
+			// Trying to get dataProvider to work with depends in phpunit requires some serious magic
+			$filePath = __DIR__ . DIRECTORY_SEPARATOR . 'GAMES.json';
+
+			$this->assertFileExists( $filePath );
+
+			return $filePath;
+		}
+
+		/**
+		* @depends testFileExists
+		*/
+		public function testFileNotEmpty( $filePath )
+		{
+			$games = file_get_contents( $filePath );
+
+			$this->assertNotEmpty( $games );
+
+			return $games;
+		}
+
+		/**
+		* We're sadistic bastards that only allow tabs
+		*
+		* @depends testFileNotEmpty
+		*/
+		public function testWhitespace( $games ) {
+
+			$this->assertNotRegExp( '/^ +/m', $games, 'Spaces used, we only allow tabs' );
+			$this->assertNotRegExp( '/^\t+ +/m', $games, 'Tabs mixed with spaces, we only allow tabs' );
+
+			$games = trim( $games );
+
+			$this->assertNotRegExp( '/\s$/m', $games, 'End of line whitespace found, fix it' );
+			$this->assertNotRegExp( '/^$/m', $games, 'Empty line found, fix it' );
+
+			return $games;
+		}
+
+		/**
+		* @depends testWhitespace
+		*/
+		public function testJSON( $games ) {
+
+			try	{
+				$parser = new JsonParser();
+				$games = $parser->parse( $games, JsonParser::DETECT_KEY_CONFLICTS + JsonParser::PARSE_TO_ASSOC );
+
+			} catch ( Exception $e ) {
+				$this->assertTrue( 'parsing', $e->getMessage() );
+			}
+
+			$allowedKeys = Array(
+				'id'		=> 'is_set',
+				'title'		=> 'is_string',
+				'note'		=> 'is_string',
+				'mapping'	=> 'is_array'
+			);
+
+			foreach( $games as $appID => $keys ) {
+
+				$this->assertTrue( is_numeric( $appID ), 'Key "' . $appID . '" must be numeric' );
+
+				if ( is_array( $keys ) ) {
+
+					$this->assertNotEmpty( $keys, '"' . $appID . '" can not be an empty array' );
+
+					foreach( $keys as $key => $value ) {
+
+						$this->assertArrayHasKey( $key, $allowedKeys, 'Invalid key "' . $key . '" in "' . $appID . '"' );
+						$this->assertTrue( $allowedKeys[ $key ]( $value ), '"' . $key . '" in "' . $appID . '" is not "' . $allowedKeys[ $key ] . '"' );
+
+						if ( $key === 'id' ) {
+
+							if ( is_array( $value ) ) {
+
+								$this->assertNotEmpty( $value, '"' . $key . '" can not be an empty field' );
+								foreach( $value as $npcommid ) {
+									$this->assertTrue( is_npcommid( $npcommid ), $npcommid . ' field in "' . $key . '" should only be a string' );
+								}
+							} else {
+								$this->assertTrue( is_npcommid( $value ), $key . ' key in "' . $appID . '" should only be a string' );
+							}
+
+						} else if ( $key === 'note' ) {
+
+							$this->assertNotEmpty( $value, '"' . $key . '" can not be an empty string' );
+
+						} else if ( $key === 'mapping' ) {
+
+							// make robust test here
+							$this->assertTrue( is_array( $value ), '"' . $key . '" is not an array"' );
+						}
+					}
+				} else {
+					$this->assertTrue( false, 'Key "' . $appID . '" has an invalid value' );
+				}
+			}
+
+			return $games;
+		}
+
+		/**
+		* @depends testJSON
+		*/
+		public function testSorting( $games ) {
+
+			$gamesSorted = $games;
+
+			ksort( $gamesSorted );
+
+			if ( $games !== $gamesSorted ) {
+
+				$gamesKeys = array_keys( $games );
+				$gamesSortedKeys = array_keys( $gamesSorted );
+				$cachedCount = count( $gamesKeys );
+
+				unset( $games, $gamesSorted );
+
+				for( $i = 0; $i < $cachedCount; ++$i ) {
+
+					$message = '';
+					if ( $gamesKeys[ $i ] !== $gamesSortedKeys[ $i ] ) {
+
+						$where = array_search( $gamesKeys[ $i ], $gamesSortedKeys ) - array_search( $gamesSortedKeys[ $i ], $gamesKeys );
+						$message = $where > 0 ? $gamesKeys[ $i ] . '" is far too early' : ( $where == 0 ? $gamesKeys[ $i ] . '" is on an adjacent line' : $gamesSortedKeys[ $i ] . '" is far too late' );
+					}
+					$this->assertEquals( $gamesKeys[ $i ], $gamesSortedKeys[ $i ], 'File must be sorted correctly by appid, "' . $message );
+				}
+			}
+		}
+	}
